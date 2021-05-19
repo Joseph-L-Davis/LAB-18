46e2d9e6b09ff3146187f9d2de55c406ec45fa85
Joseph Davis <joethebartenderpdx@gmail.com>
2021-05-19 14:20:31 -0700

    API added

--- src/app/App.js
+++ src/app/App.js
@@ -10,7 +10,31 @@ import {
 } from 'react-router-dom';
 import './App.css';
 
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://tasty.p.rapidapi.com/recipes/list");
+
+req.query({
+	"from": "0",
+	"size": "20",
+	"tags": "under_30_minutes"
+});
+
+req.headers({
+	"x-rapidapi-key": "89f81e7bd5mshd5de49e06c53005p1bc150jsnf985c53dffbe",
+	"x-rapidapi-host": "tasty.p.rapidapi.com",
+	"useQueryString": true
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
 class App extends Component {
+
+  
 
   render() {
     return (
