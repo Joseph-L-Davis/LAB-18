78617ffa335a08ef93d8941fc53f3d50cafcc012
austinxduong <axduong2@gmail.com>
2021-05-20 10:42:31 -0700

    fun timeeee yayyyyayaya

--- src/app/App.js
+++ src/app/App.js
@@ -10,67 +10,87 @@ import {
 } from 'react-router-dom';
 import './App.css';
 
-var unirest = require("unirest");
+var unirest = require('unirest');
 
-var req = unirest("GET", "https://tasty.p.rapidapi.com/recipes/list");
+var req = unirest('GET', 'https://tasty.p.rapidapi.com/recipes/list');
 
 req.query({
-	"from": "0",
-	"size": "20",
-	"tags": "under_30_minutes"
+  'from': '0',
+  'size': '20',
+  'tags': 'under_30_minutes'
 });
 
 req.headers({
-	"x-rapidapi-key": "89f81e7bd5mshd5de49e06c53005p1bc150jsnf985c53dffbe",
-	"x-rapidapi-host": "tasty.p.rapidapi.com",
-	"useQueryString": true
+  'x-rapidapi-key': '89f81e7bd5mshd5de49e06c53005p1bc150jsnf985c53dffbe',
+  'x-rapidapi-host': 'tasty.p.rapidapi.com',
+  'useQueryString': true
 });
 
 
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
+req.end(function(res) {
+  if (res.error) throw new Error(res.error);
 
-	console.log(res.body);
+  console.log(res.body);
 });
 class App extends Component {
 
+
+state = {
+  token: window.localStorage.getItem('TOKEN'),
+  userId: window.localStorage.getItem('USER_ID'),
+  userName: window.localStorage.getItem('USER_NAME')
+}
+
+handleUser = user => {
+  window.localStorage.setItem('USER_NAME', user.name);
+  window.localStorage.setItem('USER_ID', user.id);
+  window.localStorage.setItem('TOKEN', user.token);
+
+  this.setState({ token: user.token });
+
+
+}
   
 
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Header/>
-          <main>
+render() {
+  const { token, userName } = this.state;
 
-            <Switch>
-              <Route path="/" exact={true}
-                render={routerProps => (
-                  <Home {...routerProps}/>
-                )}
-              />
 
-              <Route path="/resources" exact={true}
-                render={routerProps => (
-                  <div>Implement a page of resources</div>
-                )}
-              />
+  return (
+    <div className="App">
+      <Router>
+        <Header userName={userName}/>
 
-              <Route path="/resources/:id"
-                render={routerProps => (
-                  <div>Implement a page for id {routerProps.match.params.id}</div>
-                )}
-              />
+        <main>
 
-              <Redirect to="/" />
+          <Switch>
+            <Route path="/" exact={true}
+              render={routerProps => (
+                <Home {...routerProps}/>
+              )}
+            />
 
-            </Switch>
-          </main>
-          <Footer/>
-        </Router>
-      </div>
-    );
-  }
+            <Route path="/resources" exact={true}
+              render={routerProps => (
+                <div>Implement a page of resources</div>
+              )}
+            />
+
+            <Route path="/resources/:id"
+              render={routerProps => (
+                <div>Implement a page for id {routerProps.match.params.id}</div>
+              )}
+            />
+
+            <Redirect to="/" />
+
+          </Switch>
+        </main>
+        <Footer/>
+      </Router>
+    </div>
+  );
+}
 
 }
 
--- src/app/Header.js
+++ src/app/Header.js
@@ -1,13 +1,26 @@
 import { Component } from 'react';
 import './Header.css';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
 
   render() { 
+
+    const { userName } = this.props;
+
     return (
       <header className="Header">
 
-        <h1>React App</h1>
+        <h1>Tasty Yummy</h1>
+        <nav>
+          <NavLink to="/" exact={true}> Home </NavLink>
+          <NavLink to="/recipes" exact={true}> Search Recipes </NavLink>
+          <NavLink to="/favorites" exact={true}> My Favorites </NavLink>
+          { userName
+            ? <span> Hi { userName } </span>
+            : <NavLink to="/auth"> sign in </NavLink>
+          }
+        </nav>
         
       </header>
     );
